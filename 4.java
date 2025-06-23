f81e409c5921b672b53d78e527100fe5c128ae64
123456789101088 <amtul5158@gmail.com>
2025-06-23 19:08:42 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 27;
+        if(n%2==0){
+            System.out.println("n is even");
+        }
+        else{
+            System.out.println("n is odd");
+        }
+    }
+}
+answer:n is odd
